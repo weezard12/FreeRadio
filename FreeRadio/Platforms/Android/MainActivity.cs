e3aee7784d413854c8e031e16b74a0ed100e15e9
weezard12 <weezard1234@gmail.com>
2025-03-08 21:03:16 +0200

    added fullscreen on android

--- FreeRadio/Platforms/Android/MainActivity.cs
+++ FreeRadio/Platforms/Android/MainActivity.cs
@@ -1,10 +1,27 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace FreeRadio;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Make the app Full-Screen
+        Window.SetFlags(WindowManagerFlags.Fullscreen, WindowManagerFlags.Fullscreen);
+
+        // Hide the status bar (optional)
+        Window.DecorView.SystemUiVisibility = (StatusBarVisibility)(
+            SystemUiFlags.LayoutStable |
+            SystemUiFlags.LayoutFullscreen |
+            SystemUiFlags.HideNavigation |
+            SystemUiFlags.Fullscreen |
+            SystemUiFlags.ImmersiveSticky
+        );
+    }
 }
